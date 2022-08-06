f8bddd011c0f063ff719aba9f69fcf6dbef5cb71
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-08-06 14:31:57 +0530

    :tada: Boilerplate setup

---
+++ src/client/src/App.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <div>Client running</div>;
+}
---
+++ src/server/index.ts
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express, { Request, Response } from 'express';
+import path from 'path';
+import { AppRouter, ErrorMiddleware } from '@varuntiwari/express-ts-decorators';
+
+const app = express();
+
+process.on('uncaughtException', (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Server shutting down due to uncaught exception`);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (err: Error) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`Server shutting down due to unhandled promise rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+app.use(express.json());
+
+app.use(AppRouter.getInstance());
+
+app.use(ErrorMiddleware);
+
+if (process.env.NODE_ENV === 'production') {
+  const __directory = path.resolve();
+  app.use(express.static(path.join(__directory, '../client/dist')));
+  app.get('*', (req: Request, res: Response) => {
+    res.sendFile(path.resolve(__directory, '../client', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req: Request, res) => {
+    res.send('API service running 🚀');
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+  console.log(`server running at port: ${PORT} 🚀`);
+});

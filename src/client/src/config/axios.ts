afffb999786cd0749bc1f46d308037c008e4e197
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-08-07 01:19:56 +0530

    :rocket: Heroku deploy

--- src/client/src/config/axios.ts
+++ src/client/src/config/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const customFetch = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL:
+    process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
 });

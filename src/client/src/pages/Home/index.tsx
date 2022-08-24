34ee014800cff2bc5df1be299625433c9a777d0f
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2022-08-24 18:33:22 +0530

    :sparkles: Lorem ipsum fixed

--- src/client/src/pages/Home/index.tsx
+++ src/client/src/pages/Home/index.tsx
@@ -43,37 +43,46 @@ export function Home(): JSX.Element {
         alignItems={'center'}
         spacing={{ base: 8, md: 12 }}
       >
-        <Heading
-          className='hero'
-          textAlign={'center'}
-          color={'gray.800'}
-          fontWeight={800}
-          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}
+        <Container
+          maxW={'4xl'}
+          display={'flex'}
+          flexDir={'column'}
+          alignItems={'center'}
         >
-          Create your Profile README <br />
-          <Text as={'span'} color={'orange.400'}>
-            using the best Templates
+          <Heading
+            className='hero'
+            pb={6}
+            textAlign={'center'}
+            color={'gray.800'}
+            fontWeight={800}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}
+          >
+            Create your Profile README <br />
+            <Text as={'span'} color={'orange.400'}>
+              using the best Templates
+            </Text>
+          </Heading>
+          <Text
+            mb={4}
+            textAlign={'center'}
+            fontSize={{ base: 'sm', md: 'xl' }}
+            color={'gray.500'}
+          >
+            Our simple editor allows you to quickly add and customize all the
+            sections you need for your profile's readme
           </Text>
-        </Heading>
-        <Text
-          textAlign={'center'}
-          fontSize={{ base: 'sm', md: 'xl' }}
-          color={'gray.500'}
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse labore,
-          repellat excepturi rerum nesciunt commodi a soluta illo fuga ipsa.
-        </Text>
-        <Button
-          as={Link}
-          to='/create'
-          px={{ base: '20px', md: '40px' }}
-          py={{ base: '15px', md: '30px' }}
-          fontSize={{ base: 'sm', md: 'lg' }}
-          colorScheme={'orange'}
-        >
-          Get Started
-        </Button>
+          <Button
+            as={Link}
+            to='/create'
+            px={{ base: '20px', md: '40px' }}
+            py={{ base: '15px', md: '30px' }}
+            fontSize={{ base: 'sm', md: 'lg' }}
+            colorScheme={'orange'}
+          >
+            Get Started
+          </Button>
+        </Container>
         <Box w={'100%'} height={'100%'} position={'relative'}>
           <Container maxW={'4xl'}>
             <Image src={bannerHero} boxShadow='2xl' rounded={4} />

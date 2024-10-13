1355d83e7e9286e3975bbf5bdcceb144b55c7b3f
Varun Kumar Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2024-10-13 23:33:13 +0530

    Updated src/server/utils/constants.ts

--- src/server/utils/constants.ts
+++ src/server/utils/constants.ts
@@ -1,5 +1,5 @@
 export const STREAKS_BASE_URL =
-  'https://github-readme-streak-stats-2.herokuapp.com';
+  'https://streak-stats.demolab.com';
 export const CONTRIBUTION_BASE_URL =
   'https://github-readme-stats.vercel.app/api';
 export const SOCIAL_ICONS_BASE_URL =
